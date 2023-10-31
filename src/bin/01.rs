1c931eb215374d5755ec27183b8da55216336f22
Willi Meierhof <16141053+Wilhelmsson177@users.noreply.github.com>
2023-10-31 13:00:54 +0100

    feat: day01

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,35 @@
+use itertools::Itertools;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    input
+        .lines()
+        .map(|v| v.parse::<u32>().ok())
+        .batching(|it| {
+            let mut sum = None;
+            while let Some(Some(v)) = it.next() {
+                sum = Some(sum.unwrap_or(0) + v);
+            }
+            sum
+        })
+        .max()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut calories: Vec<u32> = input
+        .lines()
+        .map(|v| v.parse::<u32>().ok())
+        .batching(|it| {
+            let mut sum = None;
+            while let Some(Some(v)) = it.next() {
+                sum = Some(sum.unwrap_or(0) + v);
+            }
+            sum
+        })
+        .collect::<Vec<u32>>();
+    print!("{calories:?}");
+    calories.sort();
+    calories.reverse();
+    Some(calories[..3].iter().sum())
 }
 
 advent_of_code::main!(1);
@@ -15,12 +41,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", 1));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(24000));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", 1));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(45000));
     }
 }
